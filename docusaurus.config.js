d7bc30c991d5bb41c0a15602e37860c3661fb1f6
silverqx <silver.zachara@gmail.com>
2021-07-11 09:54:05 +0200

    initial commit
    Docs moved to Docusaurus and I set up everything what I wanted to.

---
+++ docusaurus.config.js
@@ -0,0 +1,82 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'TinyORM',
+  tagline: 'c++ ORM library for Qt framework',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'silverqx', // Usually your GitHub org/user name.
+  projectName: 'TinyORM', // Usually your repo name.
+  themeConfig: {
+    hideableSidebar: true,
+    navbar: {
+      title: 'TinyORM',
+      logo: {
+        alt: 'TinyORM Logo',
+        src: 'img/logo.svg',
+      },
+      hideOnScroll: true,
+      items: [
+        // {
+        //   type: 'doc',
+        //   docId: 'intro',
+        //   position: 'left',
+        //   label: 'Tutorial',
+        // },
+        {
+          href: 'https://github.com/silverqx/TinyORM',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} <a href="https://github.com/silverqx" target="_blank">Silver Zachara</a>`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+      defaultLanguage: 'cpp',
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/silverqx/TinyORM-github.io/edit/main/',
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  // stylesheets: [
+  //   "https://fonts.googleapis.com/icon?family=Material+Icons",
+  // ],
+  plugins: [
+    function(context, options) {
+      return {
+        name: 'webpack-configuration-plugin',
+        configureWebpack(config, isServer, utils) {
+          return {
+            resolve: {
+              symlinks: false,
+            },
+          };
+        }
+      };
+    },
+  ],
+};
