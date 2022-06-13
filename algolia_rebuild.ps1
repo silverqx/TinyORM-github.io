81433f9711d4a4ab13cc64e273666fa353876291
silverqx <silver.zachara@gmail.com>
2022-06-13 10:39:01 +0200

    enhanced algolia_rebuild.ps1 script
    Auto source dotenv.ps1 if it's not already there.

--- algolia_rebuild.ps1
+++ algolia_rebuild.ps1
@@ -1,1 +1,12 @@
+#!/usr/bin/env pwsh
+
+Set-StrictMode -Version 3.0
+
+# Initialize docsearch-scraper environment if it's not already there
+if ((Test-Path .\dotenv.ps1) -and -not (Test-Path env:CONFIG) -and
+    -not (Test-Path env:APPLICATION_ID) -and -not (Test-Path env:API_KEY)
+) {
+    . .\dotenv.ps1
+}
+
 docker run --rm -it -e APPLICATION_ID -e API_KEY -e CONFIG @args algolia/docsearch-scraper
