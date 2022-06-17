ef85abc1380cda7fd7b58fcaa4bc97eded6e6133
silverqx <silver.zachara@gmail.com>
2022-06-17 11:59:36 +0200

    sync, integrate alternative installation methods
     - added zsh, zsh_label constants

--- src/theme/constants.js
+++ src/theme/constants.js
@@ -3,10 +3,12 @@ export const shell = 'shell'
 export const application = 'application'
 export const bash = 'bash'
 export const pwsh = 'pwsh'
+export const zsh = 'zsh'
 
 export const application_label = 'application'
 export const bash_label = 'bash'
 export const pwsh_label = 'pwsh'
+export const zsh_label = 'zsh'
 
 export const application_default = 'TestApp'
 export const bash_default = '$HOME/Code/c/'
