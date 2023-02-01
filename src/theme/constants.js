84ecc746f8f95c28ce75ea12365711f591c2c08b
silverqx <silver.zachara@gmail.com>
2023-02-01 10:56:38 +0100

    added comments

--- src/theme/constants.js
+++ src/theme/constants.js
@@ -1,15 +1,19 @@
+// Tabs group IDs
 export const shell = 'shell'
 
+// TabItem values
 export const application = 'application'
 export const bash = 'bash'
 export const pwsh = 'pwsh'
 export const zsh = 'zsh'
 
+// TabItem labels
 export const application_label = 'application'
 export const bash_label = 'bash'
 export const pwsh_label = 'pwsh'
 export const zsh_label = 'zsh'
 
+// Others
 export const application_default = 'TestApp'
 export const bash_default = '$HOME/Code/c/'
 export const pwsh_default = '$env:USERPROFILE\\Code\\c\\'
