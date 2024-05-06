d947230f8157fa2c8f35f119efba22f383be3008
silverqx <silver.zachara@gmail.com>
2024-05-06 18:31:22 +0200

    enhanced/sync docusaurus.config.js
     - synced/reverted all changes for easier upgrades (eg. added ;
       separators at end of lines, ...)
     - the goal was to make it look as similar as possible to the default
       docusaurus.config.js configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,85 +1,60 @@
-import postcssImport from 'postcss-import'
-import postcssNested from 'postcss-nested'
-import postcssNestedAncestors from 'postcss-nested-ancestors'
-import postcssPresetEnv from 'postcss-preset-env'
-import { themes as prismThemes } from 'prism-react-renderer'
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import postcssImport from 'postcss-import';
+import postcssNested from 'postcss-nested';
+import postcssNestedAncestors from 'postcss-nested-ancestors';
+import postcssPresetEnv from 'postcss-preset-env';
+import {themes as prismThemes} from 'prism-react-renderer';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'TinyORM',
   tagline: 'C++ ORM library for Qt framework',
-  url: 'https://www.tinyorm.org',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
   favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://www.tinyorm.org',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'silverqx', // Usually your GitHub org/user name.
   projectName: 'TinyORM', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
   titleDelimiter: '-',
   noIndex: false,
-  themeConfig: {
-    colorMode: {
-      defaultMode: 'dark',
-    },
-    navbar: {
-      hideOnScroll: true,
-      title: 'TinyORM',
-      logo: {
-        alt: 'TinyORM Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          href: 'https://github.com/silverqx/TinyORM',
-          position: 'right',
-          className: 'header-github-link',
-          'aria-label': 'TinyORM GitHub repository',
-          title: 'TinyORM GitHub repository',
-        },
-      ],
-    },
-    docs: {
-      sidebar: {
-        hideable: true,
-        autoCollapseCategories: true,
-      },
-    },
-    footer: {
-      links: [],
-      copyright:
-        `<span>Copyright © ${new Date().getFullYear()} </span>` +
-        `<a href="https://github.com/silverqx" target="_blank">Silver Zachara</a>`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-      defaultLanguage: 'cpp', // If no language defined after the ```
-      additionalLanguages: ['cmake', 'powershell', 'bash'],
-    },
-    algolia: {
-      appId: 'ML6TJ6GTSR',
-      apiKey: '2d548632681c7f359587e5671f67e636',
-      indexName: 'TinyORM-github.io',
-      contextualSearch: false,
-      // Optional: Algolia search parameters
-      // searchParameters: {},
-    },
-    metadata: [
-      // These keywords will be used when the front matter keywords option isn't defined
-      { name: 'keywords', content: 'c++ orm, tinyorm' },
-    ],
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
+
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/silverqx/TinyORM-github.io/edit/main/',
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/silverqx/TinyORM-github.io/edit/main/',
           routeBasePath: '/',
           exclude: [
             '**/assets/**',
@@ -92,7 +67,7 @@ module.exports = {
         },
         blog: false,
         theme: {
-          customCss: [require.resolve('./src/css/custom.css')],
+          customCss: './src/css/custom.css',
         },
         sitemap: {
           changefreq: 'weekly',
@@ -103,9 +78,66 @@ module.exports = {
           // Optional fields.
           anonymizeIP: false, // Should IPs be anonymized?
         },
-      },
+      }),
     ],
   ],
+
+  /* All below isn't possible to synchronize with the default docusaurus configuration as it's
+     completely different/customized. */
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark',
+      },
+      navbar: {
+        hideOnScroll: true,
+        title: 'TinyORM',
+        logo: {
+          alt: 'TinyORM Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            href: 'https://github.com/silverqx/TinyORM',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'TinyORM GitHub repository',
+            title: 'TinyORM GitHub repository',
+          },
+        ],
+      },
+      docs: {
+        sidebar: {
+          hideable: true,
+          autoCollapseCategories: true,
+        },
+      },
+      footer: {
+        links: [],
+        copyright:
+          `<span>Copyright © ${new Date().getFullYear()} </span>` +
+          `<a href="https://github.com/silverqx" target="_blank">Silver Zachara</a>`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+        defaultLanguage: 'cpp', // If no language defined after the ```
+        additionalLanguages: ['cmake', 'powershell', 'bash'],
+      },
+      algolia: {
+        appId: 'ML6TJ6GTSR',
+        apiKey: '2d548632681c7f359587e5671f67e636',
+        indexName: 'TinyORM-github.io',
+        contextualSearch: false,
+        // Optional: Algolia search parameters
+        // searchParameters: {},
+      },
+      metadata: [
+        // These keywords will be used when the front matter keywords option isn't defined
+        { name: 'keywords', content: 'c++ orm, tinyorm' },
+      ],
+    }),
   // stylesheets: [
   //   "https://fonts.googleapis.com/icon?family=Material+Icons",
   // ],
@@ -204,3 +236,5 @@ module.exports = {
     },
   ],
 };
+
+export default config;
